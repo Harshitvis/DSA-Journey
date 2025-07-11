1a74a12663aebf4b3c0dea2616cb4cef9b7e23f6
Harshitvis <140336479+Harshitvis@users.noreply.github.com>
2025-07-11 21:09:39 +0530

    Day 2: Solved 2 DSA problems

---
+++ Day_02/Rotated.cpp
@@ -0,0 +1,52 @@
+// Rotate array by K elements
+
+// Problem Statement: Given an array of integers, rotating array of elements by k elements either left or right.
+
+// Examples:
+
+// Example 1:
+// Input: N = 7, array[] = {1,2,3,4,5,6,7} , k=2 , right
+// Output: 6 7 1 2 3 4 5
+// Explanation: array is rotated to right by 2 position .
+
+// Example 2:
+// Input: N = 6, array[] = {3,7,8,9,10,11} , k=3 , left 
+// Output: 9 10 11 3 7 8
+// Explanation: Array is rotated to right by 3 position.
+
+#include <iostream>
+using namespace std;
+// Function to Reverse the array
+void Reverse(int arr[], int start, int end)
+{
+  while (start <= end)
+  {
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+// Function to Rotate k elements to right
+void Rotateeletoright(int arr[], int n, int k)
+{
+  // Reverse first n-k elements
+  Reverse(arr, 0, n - k - 1);
+  // Reverse last k elements
+  Reverse(arr, n - k, n - 1);
+  // Reverse whole array
+  Reverse(arr, 0, n - 1);
+}
+int main()
+{
+  int arr[] = {1, 2, 3, 4, 5, 6, 7};
+  int n = 7;
+  int k = 2;
+  Rotateeletoright(arr, n, k);
+  cout << "After Rotating the k elements to right ";
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+  cout << endl;
+  return 0;
+}
